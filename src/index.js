c809358b9ba723eae019daaab94d0184572503d0
Hanna <ann.sadovskaya@gmail.com>
2021-03-02 18:00:29 +0100

    feat: add decision for brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairs;
+  for(let i = 0; i < bracketsConfig.length;){
+    pairs = bracketsConfig[i].join('');
+    if (str.includes(pairs)){
+      str = str.replace(pairs, '');
+      i = 0;
+    }else{
+      i++;
+    }
+  }
+  return !str;
 }
